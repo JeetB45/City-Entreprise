81c4f5df8a040a6aa92f0e800b47d02a23a05bd9
JeetB45 <jeetbhavsar0510@gmail.com>
2025-06-25 00:15:32 +0530

    .

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import './Header.css';
-
+import logo from '../../assets/Logo.png'
 const Header = () => {
     return (
         <header className="app-header">
-            <img src="../../src/assets/Logo.png" alt="Logo" className="logo" />
+            <img src={logo} alt="Logo" className="logo" />
             <h1 className="company-name">City Enterprise</h1>
         </header>
     );
