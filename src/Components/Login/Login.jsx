66f7a5a83fa2282d7a6cf26774056b4e25c52fb3
JeetB45 <jeetbhavsar0510@gmail.com>
2025-06-25 10:24:31 +0530

    finalAuthfix

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
 import './Login.css';
@@ -7,12 +7,13 @@ import { Navigate } from 'react-router-dom';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             await signInWithEmailAndPassword(auth, email, password);
-            Navigate('/billform');
+            navigate('/billform');
         } catch (err) {
             alert('Invalid credentials');
         }
